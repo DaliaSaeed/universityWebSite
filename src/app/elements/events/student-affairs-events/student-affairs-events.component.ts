f61061690016b91a7fd37501e7b4e149f6ca5f86
Dalia Saeed <daliasaeedahmed12.com@gmail.com>
2022-03-23 09:20:18 -0700

    first commit

---
+++ src/app/elements/events/student-affairs-events/student-affairs-events.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-affairs-events',
+  templateUrl: './student-affairs-events.component.html',
+  styleUrls: ['./student-affairs-events.component.css']
+})
+export class StudentAffairsEventsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
